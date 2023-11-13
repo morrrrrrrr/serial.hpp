1d0eef202043eabab07142876691d49d815899d0
morrrrrrrr <moritz.weissenboeck@gmx.at>
2023-11-12 16:29:00 -0800

    Added Creation of hSerial to constructor

--- serial.hpp
+++ serial.hpp
@@ -5,29 +5,83 @@
 #include <windows.h>
 #include <thread>
 #include <string>
+#include <sstream>
+#include <stdexcept>
 #include <functional>
+#include <locale>
+#include <codecvt>
 
-namespace serial {
-    class SerialPort {
-        
-    public:
-        #pragma region Constructors
-        SerialPort(const std::string& port, int bRate) {
+#pragma region exceptions
+class SerialException : public std::exception {
+private:
+    std::string errorMessage;
 
+public:
+    SerialException(const std::string& message) : errorMessage("Serial Error: " + message) {}
+
+    // Override the what() function to provide a custom error message
+    const char* what() const noexcept override {
+        return errorMessage.c_str();
+    }
+};
+#pragma endregion
+
+class SerialPort {
+    HANDLE hSerial;
+    std::stringstream read;
+public:
+    #pragma region Constructors
+    SerialPort(const std::string& port, int bRate) {
+
+        std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+        std::wstring wPort = converter.from_bytes(port);
+
+        const WCHAR* com = wPort.c_str();
+        HANDLE hSerial = CreateFile(
+            com,
+            GENERIC_READ | GENERIC_WRITE,
+            0,
+            0,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            0
+        );
+
+        if (hSerial == INVALID_HANDLE_VALUE) {
+            throw SerialException("Error opening serial port.");
         }
 
-        #pragma endregion
+        DCB dcbSerialParams = { 0 };
+        dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
 
-        #pragma region open / close
-        void open() {
-
+        if (!GetCommState(hSerial, &dcbSerialParams)) {
+            CloseHandle(hSerial);
+            throw SerialException("Error getting serial port state.");
         }
 
-        void close() {
+        dcbSerialParams.BaudRate = CBR_9600;  // Set to match Arduino baud rate
+        dcbSerialParams.ByteSize = 8;
+        dcbSerialParams.StopBits = ONESTOPBIT;
+        dcbSerialParams.Parity   = NOPARITY;
 
+        if (!SetCommState(hSerial, &dcbSerialParams)) {
+            CloseHandle(hSerial);
+            throw SerialException("Error setting serial port state.");
         }
-        #pragma endregion
-    }; // class port
-}; // namespace serial
+
+        //hSerial is now set up
+    }
+    #pragma endregion
+
+    #pragma region open / close
+    void open() {
+
+    }
+
+    void close() {
+
+    }
+    #pragma endregion
+}; // class port
 
 #endif
